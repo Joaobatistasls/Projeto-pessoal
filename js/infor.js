21cf6a42d1d41a31772fc501cfa17c17164ef596
João <joaobatistaoficial44@gmail.com>
2022-12-12 21:53:57 -0300

    Finalizando o projeto

---
+++ js/infor.js
@@ -0,0 +1,134 @@
+let informacoes = [
+    {
+        nome: "Nade",
+        sobrenome: "Lacerda",
+        idade: "32",
+        filhos: "3",
+        trabalho: "Sim",
+        animais: "2",
+        fotos: {
+            url: "mae.jpg"
+        }
+    },
+    {
+        nome: "Poliana",
+        sobrenome: "Lacerda",
+        idade: "13",
+        filhos: "0",
+        trabalho: "Não",
+        animais: "2",
+        fotos: {
+            url: "poli.jpg"
+        }
+    },
+    {
+        nome: "Rubia",
+        sobrenome: "Calvacante",
+        idade: "48",
+        filhos: "2",
+        trabalho: "Sim",
+        animais: "2",
+        fotos: {
+            url: "titia.jpg"
+        }
+    },
+    {
+        nome: "Ronaldo",
+        sobrenome: "Serafim",
+        idade: "51",
+        filhos: "2",
+        trabalho: "Sim",
+        animais: "2",
+        fotos: {
+            url: "titio.jpg"
+        }
+    },
+    {
+        nome: "Miriam",
+        sobrenome: "Lira",
+        idade: "55",
+        filhos: "1",
+        trabalho: "Sim",
+        animais: "0",
+        fotos: {
+            url: "mira.jpg"
+        }
+    },
+    {
+        nome: "Vivian",
+        sobrenome: "Rafaela",
+        idade: "9",
+        filhos: "0",
+        trabalho: "Não",
+        animais: "2",
+        fotos: {
+            url: "vivian.jpg"
+        }
+    },
+    {
+        nome: "Rauany",
+        sobrenome: "Calvacante",
+        idade: "12",
+        filhos: "0",
+        trabalho: "Não",
+        animais: "1",
+        fotos: {
+            url: "rauany.jpg"
+        }
+    },
+    {
+        nome: "Rosineide",
+        sobrenome: "Calvacante",
+        idade: "40",
+        filhos: "2",
+        trabalho: "Sim",
+        animais: "1",
+        fotos: {
+            url: "rosineide.jpg"
+        }
+    },
+    {
+        nome: "Roselina",
+        sobrenome: "Calvacante",
+        idade: "68",
+        filhos: "5",
+        trabalho: "Não",
+        animais: "1",
+        fotos: {
+            url: "roselina.jpg"
+        }
+    },
+    {
+        nome: "João",
+        sobrenome: "Batista",
+        idade: "18",
+        filhos: "0",
+        trabalho: "Não",
+        animais: "0",
+        fotos: {
+            url: "joao.jpg"
+        }
+    },
+    {
+        nome: "Victor",
+        sobrenome: "Rafael",
+        idade: "24",
+        filhos: "0",
+        trabalho: "Sim",
+        animais: "1",
+        fotos: {
+            url: "victor.jpg"
+        }
+    },
+    {
+        nome: "Mayara",
+        sobrenome: "Serafim",
+        idade: "28",
+        filhos: "0",
+        trabalho: "Sim",
+        animais: "1",
+        fotos: {
+            url: "mayara.jpg"
+        }
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,58 @@
+document.querySelector(".btn").addEventListener("click", () => {
+
+    let input = document.querySelector("#searchInput").value;
+
+    if(input !== "") {
+        clearInfo();
+        showWarning("Não encontramos essa pessoa na família.")
+
+        let informacao = informacoes;
+
+        for(let i = 0; i < informacao.length; i++) {
+
+            if(input == informacao[i].nome) {    
+                let iInfo = informacao[i]
+                display({
+                    name: informacao[i].nome,
+                    lastname: informacao[i].sobrenome,
+                    age: informacao[i].idade,
+                    sons: informacao[i].filhos,
+                    job: informacao[i].trabalho,
+                    animals: informacao[i].animais,
+                    photo: informacao[i].fotos.url
+                });
+            };
+        };
+    } else {
+        clearInfo();
+    };
+});
+
+// Função para exibir a mensagem de carregando
+function showWarning(msg) {
+    document.querySelector(".warning").innerHTML = msg;
+};
+
+// Função que mostra as informações
+function display(json) {
+    showWarning("");
+
+    document.querySelector(".titulo").style.display = "none";
+    
+    document.querySelector(".photos img").setAttribute("src", `./images/${json.photo}`);
+
+    document.querySelector(".name span").innerHTML = `${json.name}`;
+    document.querySelector(".lastname span").innerHTML = `${json.lastname}`;
+    document.querySelector(".age span").innerHTML = `${json.age}`;
+    document.querySelector(".sons span").innerHTML = `${json.sons}`;
+    document.querySelector(".job span").innerHTML = `${json.job}`;
+    document.querySelector(".animals span").innerHTML = `${json.animals}`;
+
+    document.querySelector(".container").style.display = "block";
+};
+
+// Função para limpar as informações
+function clearInfo() {
+    showWarning("");
+    document.querySelector(".container").style.display = "none";
+};
